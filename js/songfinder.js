a6d55a859691d938f391f3cf6b29456a4de63974
Paul Burkard <pburkard88@gmail.com>
2015-02-17 07:15:10 -0500

    Track Search Test 3

--- js/songfinder.js
+++ js/songfinder.js
@@ -1,13 +1,21 @@
-angular.module('songfinder', [])
-	.controller('SearchController', ['$scope', '$http', function($scope, $http) {
+angular.module('songfinder', ['ngResource'])
+	.controller('SearchController', ['$scope', '$resource', function($scope, $resource) {
 		$scope.search_url_base = 'https://api.spotify.com/v1/search?q=';
 		$scope.search_term = '';
 		$scope.search_results = [];
 		$scope.doSearch = function() {
+			/*
+			search_url = $scope.search_url_base + $scope.search_term + '&type=track';
 			$http.get($scope.search_url_base + $scope.search_term + '&type=track').success(function(data) {
-				$scope.results = data.results;
+				$scope.search_results = data.results;
 			});
-			return $scope.results;
+			*/
+			search_url = $scope.search_url_base + $scope.search_term + '&type=track';
+			rest_search = $resource(search_url, {}, {
+				query: {method: 'GET', params:{}, isArray: false}
+				});
+			$scope.search_results = rest_search.query();
+			return $scope.search_results;
 		};
 	}]
 );
