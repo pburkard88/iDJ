fb0145bcb763ed9048d85bd21683daf82921d7f5
pburkard88 <pburkard88@Pauls-MacBook-Pro-2.local>
2015-02-17 17:12:45 -0500

    Working Track Search
    Track Search returns Track Name, first artist, and album with links to
    them on spotify that open in a new tab

--- js/songfinder.js
+++ js/songfinder.js
@@ -1,22 +1,21 @@
-angular.module('songfinder', ['ngResource'])
-	.controller('SearchController', ['$scope', '$resource', function($scope, $resource) {
+/*angular.module('songfinder', ['spotify', 'ngResource'])
+	.config(function (SpotifyProvider) {
+    	SpotifyProvider.setClientId('171b23b89d5445eeb63a3757a6d446b1');
+    	SpotifyProvider.setRedirectUri('http://example.com/callback.html');
+    	SpotifyProvider.setScope('playlist-read-private');
+  	})
+  	*/
+angular.module('songfinder', ['spotify'])
+	.controller('SearchController', ['$scope', 'Spotify', function($scope, Spotify) {
 		$scope.search_url_base = 'https://api.spotify.com/v1/search?q=';
 		$scope.search_term = '';
 		$scope.search_results = [];
-		$scope.doSearch = function() {
-			/*
-			search_url = $scope.search_url_base + $scope.search_term + '&type=track';
-			$http.get($scope.search_url_base + $scope.search_term + '&type=track').success(function(data) {
-				$scope.search_results = data.results;
-			});
-			*/
-			search_url = $scope.search_url_base + $scope.search_term + '&type=track';
-			rest_search = $resource(search_url, {}, {
-				query: {method: 'GET', params:{}, isArray: false}
-				});
-			$scope.search_results = rest_search.query();
-			return $scope.search_results;
-		};
+		$scope.searchTrack = function () {
+      		Spotify.search($scope.search_term, 'track').then(function (data) {
+        		$scope.search_results = data.tracks.items;
+      		});
+      		return $scope.search_results;
+    	};
 	}]
 );
 			
